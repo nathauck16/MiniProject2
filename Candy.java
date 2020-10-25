5ec0f2c241fd63e6cc66a50ed686e906df0bc108
haucknh <haucknh@mail.uc.edu>
2020-10-25 17:29:49 +0000

    Initial commit

---
+++ Candy.java
@@ -0,0 +1,22 @@
+public class Candy{
+
+//variables
+public String name;
+
+Candy(){
+
+  name = "";
+
+}
+
+Candy(String aName){
+  name = aName;
+}
+
+/*
+Candy(String aName){
+  name = "";
+}
+*/
+
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    
+    Candy candy1 = new Candy("Snickers");
+    System.out.println("Candy 1 is " + candy1.name);
+  }
+}
