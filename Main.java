2368d53272e7129b96ed8335f24586aaad54ff99
haucknh <haucknh@mail.uc.edu>
2020-10-27 21:26:04 +0000

    added while statement

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     //set candy 1 name
@@ -8,7 +10,13 @@ class Main {
     System.out.println("Candy 1 is : " + candy1.name);
    // Candy.printName;
 
+   //scanner to choose # of houses
+   
+    Scanner s = new Scanner(System.in);
+    System.out.println("How many houses should we trick or treat at?");
+    int houseNum = s.nextInt;
     
+    //Pillowcase getCandy;
 
   }
 }
--- Pillowcase.java
+++ Pillowcase.java
@@ -41,21 +41,38 @@ public int getCount(){
 //random number between 1-100
 public void getCandy(){
 
-Candy candyNew = new Candy();
+Candy c = new Candy();
 System.out.println("Trick or Treat!");
 
-    Random r = new Random();
-    int randomInt = r.nextInt(100) + 1;
 
-if (randomInt <= 10) {
-  candyNew.setName();
-} else if (rand < .1 + .3) {
-  return 2;
-} else {
-  return 3;
-}
+   int i = 0;
+   Random r = new Random();
+   int randomInt = r.nextInt(100) + 1;
+
+   while (i <= houseNum){
+        if (randomInt <= 10) {
+          c.setName("M&Ms");
+        } else if (randomInt <= 30) {
+          c.setName("Pink Starburst");
+        } else if (randomInt <= 47){
+          c.setName("Hershey's Bar");
+        } else if (randomInt <= 55){
+          c.setName("Milky Way");
+        } else if (randomInt <= 68){
+          c.setName("Twix");
+        } else if (randomInt <= 93){
+          c.setName("Reese's");
+        } else if (randomInt > 93){
+          c.setName("Kit Kat");
+        }
+        i = i + 1;
+   }
 }
 
+public void printNumCandies(){
+
+
+}
 
 
 }
