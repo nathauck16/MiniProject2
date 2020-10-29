65bf546e54ee4400cf947e68e16749a0a091e981
haucknh <haucknh@mail.uc.edu>
2020-10-29 15:39:24 +0000

    nothin

--- Main.java
+++ Main.java
@@ -10,22 +10,27 @@ class Main {
    // Candy.printName;
    //scanner to choose # of houses
 
-    Pillowcase(){}
+    Pillowcase(){
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Happy Halloween! How many houses should we trick or treat at?");
-        int houseNum = s.nextInt();
+      Scanner s = new Scanner(System.in);
+      System.out.println("Happy Halloween! How many houses should we trick or treat at?");
+      int houseNum = s.nextInt();
     
-    int i = houseNum;
+      int i = houseNum;
 
-    //run for loop specified number of times
+      //run for loop specified number of times
     while(i > 0){
       System.out.println("Trick or treat");
 
       Pillowcase.getCandy();
       
       i = i-1;
-    }  //run getCandy and update 
+    }
+    }
+
+        
+
+      //run getCandy and update 
     
   
     Candy printName;
